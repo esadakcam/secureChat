b53282eafcb1d63cc462cc773eacf800d2df19b7
Esad AKÇAM <m.esadakcam@hotmail.com>
2023-07-13 21:58:33 +0300

    Add ssl settings

--- src/main/java/com/akcam/secureChat/domain/message/AbstractMessage.java
+++ src/main/java/com/akcam/secureChat/domain/message/AbstractMessage.java
@@ -1,9 +1,7 @@
 package com.akcam.secureChat.domain.message;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
